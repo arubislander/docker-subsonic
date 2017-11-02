610b48cf2d5bb637360d87a1626759aedab28027
ArubIslander <arubislander@zonnet.nl>
2017-11-02 17:45:53 +0100

    bumped subsonic to version 6.1.2

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t arubislander/armhf-subsonic -t arubislander/armhf-subsonic:stable .
+docker build -t arubislander/armhf-subsonic:6.1 -t arubislander/armhf-subsonic -t arubislander/armhf-subsonic:stable .
